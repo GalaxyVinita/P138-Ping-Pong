54431acdbd5ba6b75680d2d10c9ed6d991949b52
GalaxyVinita <75704799+GalaxyVinita@users.noreply.github.com>
2022-06-07 19:40:55 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -3,3 +3,19 @@ function restart()
   pcscore = 0;
   loop();
 }
+
+function setup(){
+  var canvas =  createCanvas(700,600);
+  canvas.parent('canvas');
+  
+  video = createCapture(VIDEO);
+  video.size(700, 600);
+  video.hide();
+  
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
+  }
+
+  function modelLoaded() {
+    console.log('Model loaded!');
+  }
