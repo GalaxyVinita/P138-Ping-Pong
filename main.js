7c4cc33880b05c17bccc665a01a37f800d419908
GalaxyVinita <75704799+GalaxyVinita@users.noreply.github.com>
2022-06-16 15:30:06 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+rightWristX = 0;
+rightWristY = 0;
+scoreRightWrist = 0;
+
 function restart()
 {
   pcscore = 0;
@@ -18,4 +22,44 @@ function setup(){
 
   function modelLoaded() {
     console.log('Model loaded!');
+  }
+
+  function gotResult(error, results) {
+    if (error) {
+      console.log(error);
+    }
+    console.log(results);
+    objects = results;
+  }
+
+  function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+
+    rightWristY = results[0].pose.rightWrist.y;
+    rightWristX = results[0].pose.rightWrist.x;
+    scoreRightWrist =  results[0].pose.keypoints[10].score;
+    console.log(scoreRightWrist);
+  }
+}
+
+function draw(){
+
+  background(0); 
+  image(video, 0, 0, 700, 600);
+
+  fill("black");
+  stroke("black");
+  rect(680,0,20,700);
+
+  fill("black");
+  stroke("black");
+  rect(0,0,20,700);
+
+  if(scoreRightWrist > 0.2)
+  {
+    fill("red");
+    stroke("red");
+    circle(rightWristX, rightWristY, 30);
   }
